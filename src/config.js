542b9c52b9b8cc6f728387c741188037c363f481
SalesMatheus <40245219+SalesMatheus@users.noreply.github.com>
2018-12-19 21:28:48 -0200

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+global.SALT_KEY = 'chave';
+global.EMAIL_TMPL = 'Olá,<strong>{0}</strong>, welcome to my API! HAHA';
+
+module.exports = {
+    connectionString: 'link',
+    sendgridKey: 'chave'
+}
